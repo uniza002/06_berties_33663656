c9aaef8170f38ca0bddff4efb313603f9c339fea
uniza002 <uniza002@campus.goldsimths.ac.uk>
2025-11-18 15:42:34 +0000

    Added gold/smiths user to sql file

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,4 +2,5 @@
 
 USE berties_books;
 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99);
+INSERT INTO users (username, firstname, lastname, email, hashedPassword)VALUES('gold', 'Gold', 'Smiths', 'gold@smiths.ac.uk', 'smiths');
