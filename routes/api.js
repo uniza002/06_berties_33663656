f6f4d5d7135a75d9651af5ce76c5ef236c28647a
uniza002 <uniza002@campus.goldsimths.ac.uk>
2025-12-03 00:12:09 +0000

    Fixed SQL query building

--- routes/api.js
+++ routes/api.js
@@ -10,27 +10,41 @@ router.get('/books', function (req, res, next) {
     let book = req.query.search;
     let minprice = req.query.minprice;
     let maxprice = req.query.maxprice;
-    let sort = req.query.sort;
+    let sort = req.query.sort || "name";
 
-    // Define initial search query
-    sqlquery = "SELECT * FROM books WHERE name LIKE ?";
-    book = "%" + book + "%";
+    // SQL query construction
+    let sqlquery = "SELECT * FROM books";
+    let conditions = [];
+    let parameters = [];
 
-    // Add minimum price to SQL query if inputted
-    if (minprice && !isNaN(minprice)) {
-        sqlquery += " AND price >= " + minprice;
+    // Search query for book name
+    if (book) {
+        conditions.push("name LIKE ?");
+        parameters.push("%" + book + "%");
     }
 
-    // Add maximum price to SQL query if inputted
-    if (maxprice && !isNaN(maxprice)) {
-        sqlquery += " AND price <= " + maxprice;
+    // Search query for minimum price
+    if (minprice) {
+        conditions.push("price >= ?");
+        parameters.push(minprice);
+    }
+
+    // Search query for maximum price
+    if (maxprice) {
+        conditions.push("price <= ?");
+        parameters.push(maxprice);
+    }
+
+    // Append WHERE to SQL query if any conditions exist
+    if (conditions.length > 0) {
+        sqlquery += " WHERE " + conditions.join(" AND ");
     }
 
     // Append the sort type to the SQL query and sort by ascending
     sqlquery += " ORDER BY " + sort + " ASC";
     
     // Execute the sql query
-    db.query(sqlquery, book ? [book] : [], (err, result) => {
+    db.query(sqlquery, parameters, (err, result) => {
         // Return results as a JSON object
         if (err) {
             res.json(err)
